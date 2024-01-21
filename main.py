2263368ac854f9169267bcef8d8022dd792eba6e
Abdur Rahman Abul Hossain <abdurrahmanpm97@gmail.com>
2024-01-21 15:54:22 -0330

    added new origin to cors middleware

--- main.py
+++ main.py
@@ -30,14 +30,14 @@ origins = [
     "http://127.0.0.1:3000",
     "https://127.0.0.1:3000",
     "http://localhost:8000", 
-    "https://localhost:8000"
-    "http://localhost:80"
-    "https://localhost:80"
-    "http://54.166.200.70:3000"
-    "https://54.166.200.70:3000"
-    "http://54.166.200.70"
-    "http://54.166.200.70:80"
-    "https://54.166.200.70:80"
+    "https://localhost:8000",
+    "http://localhost:80",
+    "https://localhost:80",
+    "http://54.166.200.70:3000",
+    "https://54.166.200.70:3000",
+    "http://54.166.200.70",
+    "http://54.166.200.70:80",
+    "https://54.166.200.70:80",
     # Add other frontend URLs as needed
 ]
 
